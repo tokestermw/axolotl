8a7692c88f1f3f5b6db45a3d0c55fa04aef4a6f1
Motoki Wu <tokestermw@gmail.com>
2023-11-01 15:38:40 -0700

    debugging

--- src/axolotl/core/trainer_builder.py
+++ src/axolotl/core/trainer_builder.py
@@ -191,6 +191,8 @@ class AxolotlTrainer(Trainer):
     def get_eval_dataloader(
         self, eval_dataset: Optional[Dataset] = None
     ) -> Union[DataLoader, MultipackDistributedDataloader]:
+        print('self.args.eval_sample_packing', self.args.eval_sample_packing)
+        raise ValueError(f'self.args.eval_sample_packing: {self.args.eval_sample_packing}')
         if self.args.sample_packing and self.args.eval_sample_packing is not False:
             eval_dataset = (
                 eval_dataset if eval_dataset is not None else self.eval_dataset
